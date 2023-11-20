b3e6ad0cbd81cac127e30929e9c7bbe8fe53f2c9
DasuNadeema <dasunnadeema6@gmail.com>
2023-11-20 17:36:17 +0530

    initial commit

---
+++ yt-website/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "././Component/Navbar/Navbar.jsx";
+import Navbar from "././Component/Navbar/Navbar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ yt-website/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+import Channel from "./images/featured_channel.jpg";
+
+export default function () {
+  return (
+    <div className="Navbar-main">
+      <img src={Channel} alt="" />
+      <nav>
+        <ul>
+          <li>Movies</li>
+          <li>Category</li>
+          <li>Trending</li>
+          <li>Download</li>
+          <br></br>
+          <form action="#">
+            <input type="text" name="" id="" placeholder="Search" />
+          </form>
+        </ul>
+      </nav>
+    </div>
+  );
+}
