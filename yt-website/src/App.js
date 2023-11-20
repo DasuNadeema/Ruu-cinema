7f4c201c5335241ac708ffb98a141abc1e0d7759
DasuNadeema <dasunnadeema6@gmail.com>
2023-11-21 00:15:20 +0530

    Added Movies

--- yt-website/src/App.js
+++ yt-website/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "././Component/Navbar/Navbar.jsx";
 import Navbar from "././Component/Navbar/Navbar.jsx";
+import Movies from "./Component/Movies/Movies.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Movies />
     </div>
   );
 }
